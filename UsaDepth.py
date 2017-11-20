883817497e0c21a38e878f209f90d2a3f675b685
anacol <anacol@ideaventionsacademy.org>
2017-11-20 11:20:44 -0500

    Part1#2

---
+++ UsaDepth.py
@@ -0,0 +1,7 @@
+import math
+s = 8080464.3
+w = 22810
+
+A = w / s
+
+print("The answer is:",A,"km deep")
--- ch1_Project_GreatLakes.py
+++
@@ -1,3 +0,0 @@
-# Name
-# Project: Great Lakes are how big?
-# Chapter 1 Problem Set Part 1
